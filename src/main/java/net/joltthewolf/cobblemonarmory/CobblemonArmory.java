7cc552d3d4d4f39f42c8bbb887739587128b036b
JoltTheWolf <caylenalphawolf@gmail.com>
2025-09-10 06:59:53 -0600

    Initial commit

---
+++ src/main/java/net/joltthewolf/cobblemonarmory/CobblemonArmory.java
@@ -0,0 +1,21 @@
+package net.joltthewolf.cobblemonarmory;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CobblemonArmory implements ModInitializer {
+	public static final String MOD_ID = "cobblemonarmory";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Cobblemon:Armory is loading!");
+	}
+}
---
+++ src/main/java/net/joltthewolf/cobblemonarmory/CobblemonArmoryClient.java
@@ -0,0 +1,10 @@
+package net.joltthewolf.cobblemonarmory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemonArmoryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
