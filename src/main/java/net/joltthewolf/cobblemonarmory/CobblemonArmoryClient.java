e88bdd648dd7b6c4e3fc8f1223eecd590eeebc07
JoltTheWolf <caylenalphawolf@gmail.com>
2025-09-12 00:16:19 -0600

    Chest Loottables

--- src/main/java/net/joltthewolf/cobblemonarmory/CobblemonArmoryClient.java
+++ src/main/java/net/joltthewolf/cobblemonarmory/CobblemonArmoryClient.java
@@ -1,10 +1,18 @@
 package net.joltthewolf.cobblemonarmory;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.joltthewolf.cobblemonarmory.item.ModItemGroups;
+import net.joltthewolf.cobblemonarmory.item.ModItems;
+import net.joltthewolf.cobblemonarmory.util.ModLootTableModifiers;
+
+import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class CobblemonArmoryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        LOGGER.info("Cobblemon:Armory is loading!");
+        ModItemGroups.registerItemGroups();
+        ModItems.registerModItems();
+        ModLootTableModifiers.modifyLootTables();
     }
 }
--- src/main/java/net/joltthewolf/cobblemonarmory/util/ModLootTableModifiers.java
+++ src/main/java/net/joltthewolf/cobblemonarmory/util/ModLootTableModifiers.java
@@ -1,33 +1,54 @@
 package net.joltthewolf.cobblemonarmory.util;
 
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
-import net.joltthewolf.cobblemonarmory.CobblemonArmory;
 import net.joltthewolf.cobblemonarmory.item.ModItems;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 
-public final class ModLootTableModifiers {
-    private ModLootTableModifiers() {}
-
-    private static final Identifier BURIED_TREASURE = Identifier.of("minecraft", "chests/buried_treasure");
-    private static boolean loggedAny = false;
+public class ModLootTableModifiers {
+    private static final Identifier ANCIENT_CITY
+            = Identifier.of("minecraft", "chests/ancient_city");
+    private static final Identifier BURIED_TREASURE
+            = Identifier.of("minecraft", "chests/buried_treasure");
+    private static final Identifier BASTION_TREASURE
+            = Identifier.of("minecraft","chests/bastion_treasure");
 
     public static void modifyLootTables() {
-        CobblemonArmory.LOGGER.info("[{}] LootTableEvents.MODIFY: registering handler", CobblemonArmory.MOD_ID);
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
+            if(ANCIENT_CITY.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.25f)) // Drops 25% of the time
+                        .with(ItemEntry.builder(ModItems.COBBLEMON_SMITHING_UPGRADE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
-        LootTableEvents.MODIFY.register((id, tableBuilder, source, registries) -> {
-            if (!loggedAny) {
-                CobblemonArmory.LOGGER.info(
-                        "[{}] First loot table seen: {} (source={}, builtin={})",
-                        CobblemonArmory.MOD_ID, id, source.toString(), source.isBuiltin()
-                );
-
-                loggedAny = true;
+                tableBuilder.pool(poolBuilder.build());
             }
-            if (id.equals(BURIED_TREASURE)) {
-                CobblemonArmory.LOGGER.warn("[{}] >>> Saw buried_treasure: {}", CobblemonArmory.MOD_ID, id);
+
+            if(BURIED_TREASURE.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.25f)) // Drops 25% of the time
+                        .with(ItemEntry.builder(ModItems.COBBLEMON_SMITHING_UPGRADE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+
+            if(BASTION_TREASURE.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.25f)) // Drops 25% of the time
+                        .with(ItemEntry.builder(ModItems.COBBLEMON_SMITHING_UPGRADE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
             }
         });
     }
