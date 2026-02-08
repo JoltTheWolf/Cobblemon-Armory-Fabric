a721423d73ff4bcca3f158d89b4d6b10ec02d3f4
JoltTheWolf <99285641+JoltTheWolf@users.noreply.github.com>
2026-02-08 12:04:27 -0700

    Update ArmoryRecipeRegistry.java

--- src/main/java/net/joltthewolf/cobblemonarmory/recipe/ArmoryRecipeRegistry.java
+++ src/main/java/net/joltthewolf/cobblemonarmory/recipe/ArmoryRecipeRegistry.java
@@ -1,3 +1,9 @@
+/*
+ * Cobblemon: Armory
+ * Copyright (c) 2026 JoltTheWolf
+ * SPDX-License-Identifier: LicenseRef-Cobblemon-Armory
+ */
+
 package net.joltthewolf.cobblemonarmory.recipe;
 
 import net.minecraft.core.Registry;
